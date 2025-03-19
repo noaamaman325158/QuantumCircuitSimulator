2f45fff93cbb6523e328ddf4127dfc867179dc2b
noaa <noaamaman325158@gmail.com>
2025-03-19 16:07:57 +0200

    hotfix(app.py): port mismatch.

--- app/main/app.py
+++ app/main/app.py
@@ -26,7 +26,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6380))
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 try:
     redis_client = redis.Redis(
         host=REDIS_HOST,
